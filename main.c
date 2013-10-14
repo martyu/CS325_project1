24c317ea8e07aaf6331c670cfbe43eff4913d19c
martyu <marty@martyulrich.com>
2013-10-14 16:07:26 -0700

    Change initial end index of sub array to equal the initial start index, so single entries count as a possible sub array.

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ void printSubArray(struct subArrIndices indices, int array[]);
 int main(int argc, const char * argv[])
 {
 	int arr[] = {
-		536,565,-319,653,667,67,-331,297,-688,963,730,-725,298,21,797,557,286,-641,-290,-151,271,-534,730,865,986,-4,-984,762,-792,-450,428,-753,49,245,-141,-710,325,-862,294,-859,517,-911,-671,223,-577,778,916,-871,-835,541,986,593,-913,-357,-358,478,616,958,-440,-75,155,645,508,-29,-715,-275,633,-790,-441,6,98,-232,-78,-941,-67,-29,539,-791,702,-81,-477,-481,978,901,-784,563,-589,-712,-860,373,872,-315,-13,-691,-953,-602,380,834,-734,-778
+		715,-782,469,548,438,-290,-560,-916,573,-362,-654,494,62,128,-589,-22,159,674,-875,490,-393,460,317,-350,232,872,470,-443,-814,-702,0,-126,-352,-945,-905,-113,-393,248,477,-720,-546,-102,785,826,-19,-943,872,857,50,935,894,416,794,448,-769,-88,728,-935,-96,-126,847,324,924,-389,-643,-253,523,-292,-786,-974,-155,-644,-895,246,771,457,-584,-537,477,34,879,782,0,158,5,-452,211,-780,-786,-638,-819,-751,226,-392,685,764,17,916,-823,562
 	};
 
 	clock_t startTime;
@@ -40,9 +40,9 @@ int main(int argc, const char * argv[])
 
 	runTime = (double)(endTime - startTime) / (double)CLOCKS_PER_SEC;
 
-	printf("Run time of algorithm 1: %lf\n", runTime);
-
 	printSubArray(closeToZeroIndices, arr);
+
+	printf("Run time of algorithm 1: %lf\n\n", runTime);
 
 
 	// Algorithm 2
@@ -53,10 +53,9 @@ int main(int argc, const char * argv[])
 
 	runTime = (double)(endTime - startTime) / (double)CLOCKS_PER_SEC;
 
-	printf("Run time of algorithm 2: %lf\n", runTime);
-
 	printSubArray(closeToZeroIndices, arr);
 
+	printf("Run time of algorithm 2: %lf\n", runTime);
 
 	return 0;
 }
@@ -69,7 +68,7 @@ struct subArrIndices closeToZero1(int arr[])
 
 	for (int i = 0; i < 100; i++)
 	{
-		for (int j = i+1; j < 100; j++)
+		for (int j = i; j < 100; j++)
 		{
 			indices.start = i;
 			indices.end = j;
@@ -96,7 +95,6 @@ end_loop:
 
 	printf("Best subarray total found: %i\n", bestTotal);
 	printf("Best subarray indices: %i, %i\n", bestTotalIndices.start, bestTotalIndices.end);
-	printf("Values at those indices: %i, %i\n", arr[bestTotalIndices.start], arr[bestTotalIndices.end]);
 
 	return bestTotalIndices;
 }
@@ -110,7 +108,7 @@ struct subArrIndices closeToZero2(int arr[])
 	{
 		int total = arr[i];
 
-		for (int j = i+1; j < 100; j++)
+		for (int j = i; j < 100; j++)
 		{
 			total += arr[j];
 
@@ -132,7 +130,6 @@ end_loop:
 
 	printf("Best subarray total found: %i\n", bestTotal);
 	printf("Best subarray indices: %i, %i\n", bestTotalIndices.start, bestTotalIndices.end);
-	printf("Values at those indices: %i, %i\n", arr[bestTotalIndices.start], arr[bestTotalIndices.end]);
 
 	return bestTotalIndices;
 }
@@ -140,7 +137,7 @@ end_loop:
 
 void printSubArray(struct subArrIndices indices, int array[])
 {
-	printf("[");
+	printf("Close to zero sub array: [");
 	for (int i = indices.start; i <= indices.end; i++)
 	{
 		printf("%i,", array[i]);
